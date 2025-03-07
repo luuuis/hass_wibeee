4da7f325fff2b0c2f62363c60135ca3d8df17cad
Luis Miranda <161006+luuuis@users.noreply.github.com>
2025-03-07 14:50:41 +0000

    test: add pytest-homeassistant-custom-component for testing with hass

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,26 +1,16 @@
 import logging
-import os
 from datetime import timedelta
 
 import aiohttp
-import pytest
 from aioresponses import aioresponses
+from pytest_homeassistant_custom_component.common import load_fixture
 
 from wibeee import api
-
-_THIS_DIR = os.path.dirname(os.path.realpath(__file__))
-
-
-def read_file(filename: str) -> str:
-    with open(os.path.join(_THIS_DIR, filename), encoding="utf-8") as file:
-        return file.read()
-
 
 DEVICE_INFO = api.DeviceInfo(id='X', macAddr='111111111111', softVersion='4.4.124', model='WB3', ipAddr='10.10.10.100')
 TIMEOUT = timedelta(seconds=5)
 
 
-@pytest.mark.asyncio
 async def test_fetch_device_info():
     async with aiohttp.ClientSession() as session:
         with aioresponses() as m:
@@ -33,7 +23,7 @@ async def test_fetch_device_info():
             m.get(
                 "http://1.2.3.4/services/user/values.xml?var=X.macAddr&X.softVersion&X.model&X.ipAddr",
                 status=200,
-                body=read_file('test_api_values.xml'),
+                body=load_fixture('test_api_values.xml'),
             )
 
             wibeee = api.WibeeeAPI(session, '1.2.3.4', timeout=TIMEOUT)
@@ -41,7 +31,6 @@ async def test_fetch_device_info():
             assert device_info == DEVICE_INFO
 
 
-@pytest.mark.asyncio
 async def test_fetch_values(caplog):
     caplog.set_level(logging.DEBUG)
     async with aiohttp.ClientSession() as session:
@@ -49,7 +38,7 @@ async def test_fetch_values(caplog):
             m.get(
                 "http://1.2.3.4/services/user/values.xml?id=WIBEEE",
                 status=200,
-                body=read_file('test_api_values.xml'),
+                body=load_fixture('test_api_values.xml'),
             )
 
             wibeee = api.WibeeeAPI(session, '1.2.3.4', timeout=TIMEOUT)
