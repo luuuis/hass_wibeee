01d1fae0c1dd9f50bd54ae067421c473aa961275
Luis Miranda <161006+luuuis@users.noreply.github.com>
2025-03-10 13:23:37 +0000

    test: add contest.py to allow custom_components to be loaded in test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,10 @@
+import sys
+
 import pytest
 
 
 @pytest.fixture(autouse=True)
 def auto_enable_custom_integrations(enable_custom_integrations):
-    yield
+    # should not be needed but for some reason Python already has in its cache the
+    # site-packages/pytest_homeassistant_custom_component.testing_config.custom_components
+    del sys.modules['custom_components']
