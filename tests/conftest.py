3df20e3020aef14becf2e67bd0cf67226b22e063
Luis Miranda <161006+luuuis@users.noreply.github.com>
2025-03-16 09:51:46 +0000

    test: try to make tests pass in Windows

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,16 @@
+import asyncio
+import sys
+
 import pytest
 
 
 @pytest.fixture(autouse=True)
 def auto_enable_custom_integrations(enable_custom_integrations):
     pass
+
+
+@pytest.fixture(autouse=True)
+def auto_override_eventloop():
+    if sys.platform == "win32":
+        # workaround for AttributeError: 'ProactorEventLoop' object has no attribute '_ssock'
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
