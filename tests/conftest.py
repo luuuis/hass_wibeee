45e8dbc276e126100b4b30122cea3008e32b7c27
Luis Miranda <161006+luuuis@users.noreply.github.com>
2025-03-16 10:23:01 +0000

    test: try to make tests pass in Windows

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,7 +6,7 @@ import pytest
 
 @pytest.fixture(autouse=True)
 def auto_enable_custom_integrations(enable_custom_integrations):
-    pass
+    yield
 
 
 @pytest.fixture(scope='session', autouse=True)
