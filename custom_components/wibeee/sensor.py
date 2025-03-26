a0d61d3b6215d4dc4241fcdd04053a384bf4747d
Luis Miranda <161006+luuuis@users.noreply.github.com>
2025-03-26 22:26:06 +0000

    chore: minor tweaks to simplify PR

--- custom_components/wibeee/sensor.py
+++ custom_components/wibeee/sensor.py
@@ -5,7 +5,6 @@ Vendor docs: https://support.wibeee.com/space/CH/184025089/XML
 Documentation: https://github.com/luuuis/hass_wibeee/
 
 """
-from . import CONF_MAC_ADDRESS
 
 REQUIREMENTS = ["xmltodict"]
 
