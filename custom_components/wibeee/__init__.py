6d1e0350906c42a54599144aa428731059f803e0
Luis Miranda <161006+luuuis@users.noreply.github.com>
2025-04-21 20:05:09 +0100

    refactor: removed 'disposers' concept. this is all handled by entry.async_on_unload

--- custom_components/wibeee/__init__.py
+++ custom_components/wibeee/__init__.py
@@ -24,7 +24,6 @@ PLATFORMS = [Platform.SENSOR]
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up platform from a ConfigEntry."""
     hass.data.setdefault(DOMAIN, {})
-    hass.data[DOMAIN][entry.entry_id] = {'disposers': {}}
 
     _LOGGER.info(f"Setup config entry '{entry.title}' (unique_id={entry.unique_id})")
 
@@ -38,28 +37,10 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
-    disposers = hass.data[DOMAIN][entry.entry_id]['disposers']
-
-    for name, dispose in list(disposers.items()):
-        _LOGGER.debug(f"Dispose of '{name}', {dispose}")
-        try:
-            dispose()
-            disposers.pop(name)
-        except Exception:
-            _LOGGER.error(f"Dispose failure for '{name}'", exc_info=True)
-
-    _LOGGER.debug(f"Disposers finished.")
-
-    dispose_ok = len(disposers) == 0
-    unload_ok = False
-    if dispose_ok:
-        _LOGGER.debug(f"Unloading sensor entry for {entry.title} (unique_id={entry.unique_id})")
-        unload_ok = await hass.config_entries.async_forward_entry_unload(entry, "sensor")
-        if unload_ok:
-            hass.data[DOMAIN].pop(entry.entry_id)
-
+    _LOGGER.debug(f"Unloading sensor entry for {entry.title} (unique_id={entry.unique_id})")
+    unload_ok = await hass.config_entries.async_forward_entry_unload(entry, "sensor")
     _LOGGER.info(f"Unloaded config entry '{entry.title}' (unique_id={entry.unique_id})")
-    return dispose_ok and unload_ok
+    return unload_ok
 
 
 async def async_update_options(hass: HomeAssistant, entry: ConfigEntry) -> None:
